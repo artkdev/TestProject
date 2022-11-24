94841cbfe59c6e1cc54727b4b33344c6256c0f2b
Crypto Space <cryptospacede@gmail.com>
2022-11-24 20:20:01 +0100

    add amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "testAuthen": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3testauthenstorage251cf5c4": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
