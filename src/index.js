8e5e567eb71dac7b25535895cced5c9ca5148c09
George <jorjio21@gmail.com>
2022-11-25 10:43:23 +0200

    comment

--- src/index.js
+++ src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Amplify from "aws-amplify";
+
+import { Amplify } from "aws-amplify";
 import config from "./aws-exports";
+import "@aws-amplify/ui-react/styles.css";
+import { AmplifyProvider } from "@aws-amplify/ui-react";
 
 Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AmplifyProvider>
+      <App />
+    </AmplifyProvider>
   </React.StrictMode>
 );
 
